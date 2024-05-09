b6f095bfa7148ccf2ae29d8fcb3272741512d06c
d4ny4bl3 <danisj91@gmail.com>
2024-05-09 22:24:14 +0200

    Solved

---
+++ 5] dice/main.py
@@ -0,0 +1,50 @@
+from random import randint
+
+dice = ("D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100")
+
+
+def roll_the_dice(input_dice):
+    for cube in dice:
+        if cube in input_dice:
+            try:
+                number_of_rolls, modifier = input_dice.split(cube)
+            except ValueError:
+                return "Wrong Dice"
+            type_of_dice = cube[1:]
+            break
+    else:
+        return "Wrong dice"
+
+    if number_of_rolls == "":
+        number_of_rolls = 1
+
+    if modifier == "":
+        modifier = 0
+
+    number_of_rolls, type_of_dice, modifier = map(int, [number_of_rolls, type_of_dice, modifier])
+
+    print(f"number_of_rolls: {type(number_of_rolls)}")
+    print(f"type_of_dice: {type(type_of_dice)}")
+    print(f"modifier: {type(modifier)}")
+    print(f"number_of_rolls: {number_of_rolls}")
+    print(f"type_of_dice: {type_of_dice}")
+    print(f"modifier: {modifier}")
+    print()
+
+    dice_sum = 0
+    for i in range(number_of_rolls):
+        hlp = randint(1, type_of_dice)
+        print(f"hlp:{hlp}")
+        dice_sum += hlp
+    print(f"dice_sum: {dice_sum}")
+
+    calculation1 = dice_sum + modifier
+    print(f"Calculation1: {calculation1}")
+
+    calculation = sum([randint(1, type_of_dice) for i in range(number_of_rolls)]) + modifier
+
+    return f"calculation: {calculation}"
+
+
+if __name__ == "__main__":
+    print(roll_the_dice("2D6+4"))
